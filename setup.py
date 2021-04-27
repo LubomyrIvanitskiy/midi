bd458cae7541863bcd5010ebcd5f8693de65527b
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2021-04-27 18:51:31 +0300

    Setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='midiwrapper',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/LubomyrIvanitskiy/midi.git',
+    license='',
+    author='liubomyr.ivanitskyi',
+    author_email='lubomyr.ivanitskiy@gmail.com',
+    description='A helper for pretty_midi lib'
+)
