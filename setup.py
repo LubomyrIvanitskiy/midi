d674085c2bdf57ebe04b0331d5f1f1e87ebee79c
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2021-04-27 19:02:21 +0300

    Update setup.py with requirements

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ setup(
     name='midiwrapper',
     version='0.1',
     packages=[''],
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'numpy',
+        'pretty_midi',
+        'libfmp'
+    ],
     url='https://github.com/LubomyrIvanitskiy/midi.git',
     license='',
     author='liubomyr.ivanitskyi',
     author_email='lubomyr.ivanitskiy@gmail.com',
-    description='A helper for pretty_midi lib'
+    description='A helper for pretty_midi lib',
 )
